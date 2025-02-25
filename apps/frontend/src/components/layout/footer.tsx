9a3d2cf566b0474f117837a6de50e317a2cc25fe
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-02-25 10:48:22 +0800

    Fix footer

--- apps/frontend/src/components/layout/footer.tsx
+++ apps/frontend/src/components/layout/footer.tsx
@@ -1,51 +1,55 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-8 px-4">
       <div className="max-w-7xl mx-auto">
         <div className="border-b border-gray-700 pb-6 mb-6"></div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           <div>
-            <h2 className="text-xl font-bold">Kanaliiga Ry</h2>
+            <h2>Kanaliiga Ry</h2>
             <p className="mt-2">Elektronisen urheilun firmaliiga</p>
             <p className="mt-2">
               Contact us:{" "}
-              <a href="mailto:info@kanaliiga.fi" className="text-blue-400">
-                info@kanaliiga.fi
-              </a>
+              <Link href="mailto:info@kanaliiga.fi">info@kanaliiga.fi</Link>
             </p>
-            <p>You can reach us on Discord</p>
+            <p>
+              You can reach us on{" "}
+              <Link href="https://discord.gg/nhrNC9x">Discord</Link>
+            </p>
           </div>
 
           {/* Column 2 - Sponsors */}
           <div>
-            <h2 className="text-xl font-bold">Sponsors</h2>
-            <div className="mt-4 space-y-2">
+            <h2>Sponsors</h2>
+            <div className="mt-4 space-y-6 flex flex-col items-center md:items-start lg:items-center">
               <Image
                 src="https://stats.kanaliiga.fi/cs/assets/sponsor-logos/elisa-esports.png"
                 alt="Elisa Esports"
                 width={200}
-                height={50}
+                height={100}
               />
 
-              <Image
-                src="https://stats.kanaliiga.fi/cs/assets/sponsor-logos/polar_squad.png"
-                alt="Polar Squad"
-                width={200}
-                height={50}
-              />
-              <Image
-                src="https://stats.kanaliiga.fi/cs/assets/sponsor-logos/tnnet.png"
-                alt="TNNet"
-                width={200}
-                height={50}
-              />
+              <div className="flex space-x-6">
+                <Image
+                  src="https://stats.kanaliiga.fi/cs/assets/sponsor-logos/polar_squad.png"
+                  alt="Polar Squad"
+                  width={100}
+                  height={50}
+                />
+                <Image
+                  src="https://stats.kanaliiga.fi/cs/assets/sponsor-logos/tnnet.png"
+                  alt="TNNet"
+                  width={100}
+                  height={50}
+                />
+              </div>
 
               <Image
                 src="https://stats.kanaliiga.fi/cs/assets/sponsor-logos/atflow.png"
                 alt="Atflow"
-                width={200}
+                width={100}
                 height={50}
               />
             </div>
@@ -53,7 +57,7 @@ const Footer = () => {
 
           {/* Column 3 - Follow Us */}
           <div>
-            <h2 className="text-xl font-bold">Follow us</h2>
+            <h2>Follow us</h2>
             <div className="mt-4 space-y-2">
               {[
                 { name: "Discord", link: "https://discord.gg/nhrNC9x" },
@@ -73,54 +77,57 @@ const Footer = () => {
                 },
                 { name: "Instagram", link: "http://instagram.com/Kanaliiga" }
               ].map((social) => (
-                <a
-                  key={social.name}
-                  href={social.link}
-                  className="block text-blue-400 hover:underline"
-                >
+                <Link key={social.name} href={social.link} className="block">
                   {social.name}
-                </a>
+                </Link>
               ))}
             </div>
           </div>
 
-          {/* Column 4 - Credits */}
           <div>
-            <h2 className="text-xl font-bold">Credits</h2>
+            <h2>Credits</h2>
             <div className="mt-4">
-              <p>
-                Created by{" "}
-                <a
-                  href="https://www.linkedin.com/in/jari-haikonen/"
-                  className="text-blue-400"
-                >
+              <p>Created by:</p>
+              <div>
+                <Link href="https://www.linkedin.com/in/jari-haikonen/">
                   enzoj
-                </a>
-              </p>
-              <p>
-                Powered by{" "}
-                <a
-                  href="https://www.linkedin.com/in/jari-haikonen/"
-                  className="text-blue-400"
-                >
+                </Link>
+                {" & "}
+                <Link href="https://www.linkedin.com/in/sebastian-sergelius/">
+                  sububobi
+                </Link>
+                {" & "}
+                <Link href="https://www.linkedin.com/in/mika-schroderus-58191b106/">
+                  Xynte
+                </Link>
+                <div className="font-sm">
+                  and other{" "}
+                  <Link href="https://gitlab.com/kanaliiga_public/kanahub/eggosystem/-/graphs/main?ref_type=heads">
+                    contributors
+                  </Link>
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <p>Data mining by:</p>
+                <Link href="https://www.linkedin.com/in/jari-haikonen/">
                   enzoj
-                </a>
-              </p>
-              <p className="mt-2">
-                Data mining by{" "}
-                <a
-                  href="https://www.linkedin.com/in/jari-haikonen/"
-                  className="text-blue-400"
-                >
-                  enzoj
-                </a>
-              </p>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="text-center text-sm text-gray-400 mt-6">
           <p>Kanaliiga Ry / hub.kanaliiga.fi</p>
+          <p>
+            Source:{" "}
+            <Link
+              href={"https://gitlab.com/kanaliiga_public/kanahub/eggosystem"}
+            >
+              Gitlab
+            </Link>
+          </p>
         </div>
       </div>
     </footer>
