3e71749fbd3b7fd977062505ea034a50711b6265
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-02-28 11:22:24 +0800

    Try after clean runner caches

--- packages/types/src/db/index.ts
+++ packages/types/src/db/index.ts
@@ -19,8 +19,8 @@ import type { SeasonTeam } from "./SeasonTeam.interface";
 import type { SeasonTeamPlayer } from "./SeasonTeamPlayer.interface";
 import type { Team } from "./Team.interface";
 import type { TeamMapScore } from "./TeamMapScore.interface";
-import type { TeamRoster } from "./TeamRoster.interface";
 import type { League } from "./League.interface";
+import type { TeamRoster } from "./TeamRoster.interface";
 
 export type {
   Game,
