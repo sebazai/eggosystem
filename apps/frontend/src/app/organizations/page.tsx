07f8d2abf98eb6944cde743aaf8f340db4dbc087
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-03-01 13:20:14 +0800

    Try sm:grid-cols-2

--- apps/frontend/src/app/organizations/page.tsx
+++ apps/frontend/src/app/organizations/page.tsx
@@ -59,7 +59,7 @@ export default function AllOrganizations() {
       setSearchQuery={setSearchQuery}
     >
       {/* Organization Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6 justify-items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 justify-items-center">
         {filteredOrganizations.length > 0 ? (
           filteredOrganizations.map((org) => (
             <FadeOnScroll key={org.id}>
