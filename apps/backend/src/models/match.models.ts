c49eaad87306929f089ab1a6018a92117ea3d8d6
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-03-03 14:34:14 +0800

    Limit 100 if search params

--- apps/backend/src/models/match.models.ts
+++ apps/backend/src/models/match.models.ts
@@ -160,7 +160,7 @@ export const getMatchesByFilters = async ({
 
       WHERE ${query}
 
-      ORDER BY match_played_id DESC ${query === "1=1" ? "LIMIT 500" : "LIMIT 1000"};
+      ORDER BY match_played_id DESC ${query === "1=1" ? "LIMIT 500" : "LIMIT 100"};
       `;
 
   return runQuery(baseQuery, queryParams);
