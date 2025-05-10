8df4e9638e62a0c4c534aeb85aa51ea723bd82fb
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-05-10 07:34:56 +0000

    Fix CS2 premier rank stripes height

--- apps/frontend/src/components/profile/cs2-premier-rank.tsx
+++ apps/frontend/src/components/profile/cs2-premier-rank.tsx
@@ -68,7 +68,7 @@ export const CS2PremierRankBadge = ({ rankScore }: { rankScore: number }) => {
     <div className="flex cs-premier-rank">
       <svg
         viewBox="0 0 17 32"
-        className="w-[1rem] h-6 z-[1] overflow-hidden align-middle"
+        className="w-[1rem] h-[23px] z-[1] overflow-hidden align-middle"
         style={{ fill: textColor }}
       >
         <path d="M5.44 2.13A2.6 2.6 0 0 1 7.99 0h1.86a.6.6 0 0 1 .6.7L4.83 31.5a.6.6 0 0 1-.6.5h-2.3c-1 0-1.76-.9-1.58-1.89l5.1-27.98ZM11.82.99c.1-.57.6-.99 1.18-.99h2.93a.6.6 0 0 1 .59.7l-5.4 30.31c-.1.57-.6.99-1.18.99H7a.6.6 0 0 1-.59-.7L11.82.98Z" />
