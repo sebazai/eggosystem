694172287e6e53aa9bf225ecad6941ff3db54d42
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-05-16 07:33:43 +0000

    Add metadata base

--- apps/frontend/src/lib/metadata.ts
+++ apps/frontend/src/lib/metadata.ts
@@ -1,9 +1,11 @@
+import { envConfig } from "@/configs/env";
 import type { Metadata } from "next";
 
 export function createPageMetadata({ title, description }: Partial<Metadata>) {
   return {
     title,
     description,
-    applicationName: "Kanahub by Kanaliiga"
+    applicationName: "Kanahub by Kanaliiga",
+    metadataBase: new URL(envConfig.BASE_URL)
   } satisfies Metadata;
 }
