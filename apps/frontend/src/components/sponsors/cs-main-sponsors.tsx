12127732c158710231b4ad8275a3e61b1a25ebea
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-05-23 09:04:25 +0000

    Mobile landscape

--- apps/frontend/src/components/sponsors/cs-main-sponsors.tsx
+++ apps/frontend/src/components/sponsors/cs-main-sponsors.tsx
@@ -7,7 +7,7 @@ export const CsMainSponsors = () => {
     <Link href={"https://supermetrics.com/"} target="_blank">
       <Image
         src={createNextUrl("/images/sponsors/supermetrics.png")}
-        className="w-[150px] h-[23px] landscape:w-[150px] landscape:h-[23px] sm:w-[300px] sm:h-[46px] lg:landscape:w-[300px] lg:landscape:h-[46px]"
+        className="w-[150px] h-[23px] sm:w-[300px] sm:h-[46px] mobile-landscape:w-[150px] mobile-landscape:h-[23px]"
         alt="Supermetrics"
         width={150}
         height={89}
