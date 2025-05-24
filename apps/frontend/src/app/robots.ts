b3a888297acd6f5775808790dda94d178b17596c
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-05-24 02:52:25 +0000

    Try hubdev

--- apps/frontend/src/app/robots.ts
+++ apps/frontend/src/app/robots.ts
@@ -2,8 +2,9 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   const isProduction = process.env.NODE_ENV === "production";
+  const isHubDev = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 
-  return isProduction
+  return isProduction && !isHubDev.includes("hubdev")
     ? {
         rules: {
           userAgent: "*",
