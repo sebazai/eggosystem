6e56a2ef17b3f804bdadbacd70283af5c88ec8a8
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-05-29 09:03:57 +0000

    Fix hostname to allow all allstar urls

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "mediacdn.allstar.gg"
+        hostname: "*.allstar.gg"
       }
     ]
   }
