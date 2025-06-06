6547b470601ab4d978bfca774f8fa12a69486623
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-06-06 02:00:45 +0000

    Pass error into winston

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -21,6 +21,7 @@ import { landingPageStatistics } from "../services/landing-page.services";
 import parseQueryFilterParams from "../middlewares/parse-query-filter-params.middleware";
 import { cacheResponseMiddleware } from "../middlewares/cache-filtered-queries";
 import { logger } from "../utils/app-logger";
+import { NotFoundError } from "../utils/errors";
 
 // Create a new Router instance
 const v1Router = Router();
@@ -53,7 +54,7 @@ v1Router.use("/faceit", faceitRouter);
 v1Router.use("/allstar", allstarRouter);
 
 v1Router.get("/stats", async (req, res) => {
-  logger.error("Stats endpoint accessed");
+  logger.error("Stats endpoint accessed", new NotFoundError("Testing errors"));
   const stats = await landingPageStatistics();
   res.status(200).json(stats);
 });
