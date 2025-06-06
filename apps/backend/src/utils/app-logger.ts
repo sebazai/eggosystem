a96d07ace9897cf08abbbd40754911dd992ec8a3
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-06-06 02:19:52 +0000

    See how it looks in grafana

--- apps/backend/src/utils/app-logger.ts
+++ apps/backend/src/utils/app-logger.ts
@@ -57,6 +57,7 @@ export const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
     winston.format.timestamp(),
+    winston.format.colorize(),
     winston.format.errors({ stack: true }),
     winston.format.splat(),
     winston.format.printf(({ level, message, timestamp, stack }) => {
