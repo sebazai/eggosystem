ef8105de6e0619204a51a10fe52aaf63761a05f3
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-08-19 14:19:59 +0800

    Fix query

--- apps/backend/src/models/season-league-external-id.models.ts
+++ apps/backend/src/models/season-league-external-id.models.ts
@@ -23,7 +23,7 @@ export const insertSeasonLeagueExternalId = async (
   isBO2PlayedAs2xBO1: boolean,
   connection?: PoolConnection
 ) => {
-  const query = `INSERT INTO SeasonLeagueExternalIds (external_id, external_league_name, season_id, league_id, stage, type, isBO2PlayedAs2xBO1) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  const query = `INSERT INTO SeasonLeagueExternalIds (external_id, external_league_name, season_id, league_id, stage_id, type, isBO2PlayedAs2xBO1) VALUES (?, ?, ?, ?, ?, ?, ?)`;
   const seasonLeagueExternaMatchRoomResult = await runQuery<{
     insertId: number;
   }>(
