2891cac4be2f93ad3613995817f63414233d1c9a
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-08-25 03:16:58 +0000

    Update dev seed and team id in dev seed

--- apps/backend/migrations/20250825023852_add_fk_to_teams_table_season_team_players.ts
+++ apps/backend/migrations/20250825023852_add_fk_to_teams_table_season_team_players.ts
@@ -2,7 +2,12 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable("SeasonTeamPlayers", (table) => {
-    table.foreign("team_id").references("id").inTable("Teams");
+    table
+      .foreign("team_id")
+      .references("id")
+      .inTable("Teams")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
   });
 }
 
