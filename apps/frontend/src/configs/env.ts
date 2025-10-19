69cc7ca6fe56b6c31f5189dc3fb66a95ddd48ff9
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-10-19 08:31:52 +0000

    Add 2dviewer api urls to frontend env

--- apps/frontend/src/configs/env.ts
+++ apps/frontend/src/configs/env.ts
@@ -1,8 +1,9 @@
 export const envConfig = {
+  VIEWER_API_URL:
+    process.env.NEXT_PUBLIC_VIEWER_API_URL ?? "http://localhost:3002",
   API_URL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001",
   CLIENT_API_URL:
     process.env.NEXT_PUBLIC_CLIENT_API_URL ?? "http://localhost:3001",
   BASE_URL: process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000",
-  BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH ?? "",
-  API_KEY: process.env.NEXT_PUBLIC_API_KEY ?? "development_api_key"
+  BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH ?? ""
 };
