d7da6a07c39d21cadb7306a90c95775f2eb6bb5b
Haikonen Jari <jari.haikonen@gmail.com>
2025-12-05 09:21:09 +0000

    fmt

--- apps/backend/jest.config.js
+++ apps/backend/jest.config.js
@@ -41,13 +41,13 @@ const isShardedCI =
 export const coverageThreshold = isShardedCI
   ? undefined
   : {
-    global: {
-      branches: 65,
-      functions: 50,
-      lines: 60,
-      statements: 60
-    }
-  };
+      global: {
+        branches: 65,
+        functions: 50,
+        lines: 60,
+        statements: 60
+      }
+    };
 export const moduleNameMapper = {
   "^@eggosystem/types$": "<rootDir>/../../packages/types/dist/index.js",
   "^@eggosystem/shared-msw$":
