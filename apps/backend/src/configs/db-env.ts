54533f39ad1fd643f2cdab0b29907ae75529ad81
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2025-12-23 01:48:09 +0000

    Set timezone utc in knex

--- apps/backend/src/configs/db-env.ts
+++ apps/backend/src/configs/db-env.ts
@@ -1,7 +1,12 @@
+import type knex from "knex";
+
 export const dbEnvConfig = {
   host: process.env.DB_HOST ?? "eggo-devdb",
   port: parseInt(process.env.DB_PORT ?? "3306"),
   user: process.env.DB_USER ?? "kanadbuser",
   password: process.env.DB_PASSWORD ?? "dev-pass",
-  database: process.env.DB_NAME ?? "kanaliiga"
-};
+  database: process.env.DB_NAME ?? "kanaliiga",
+  timezone: "utc"
+} satisfies
+  | knex.Knex.StaticConnectionConfig
+  | knex.Knex.ConnectionConfigProvider;
