288405365a7c2d646e0e7924e7f21400a3c684c1
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2026-01-11 23:14:31 +0000

    Add dashboard season registration tests

--- packages/types/src/seasons/SeasonDetails.interface.ts
+++ packages/types/src/seasons/SeasonDetails.interface.ts
@@ -1,5 +1,35 @@
-import { Game, Season } from "@eggosystem/types";
+import { Game, Season, SeasonPlatform } from "@eggosystem/types";
 
 export interface SeasonDetails extends Season {
   app_id: Game["app_id"];
 }
+
+export const createMockSeasonDetails = (
+  overrides?: Partial<SeasonDetails>
+): SeasonDetails => {
+  return {
+    id: 1,
+    game_id: 1,
+    game_type_id: 1,
+    organizer_id: 1,
+    name: "Season 1",
+    full_name: "Test Season 1",
+    app_id: 730,
+    signup_start_date: "2024-01-01T00:00:00Z",
+    signup_end_date: "2024-01-31T23:59:59Z",
+    start_date: "2024-02-01",
+    end_date: "2024-03-31",
+    platform: SeasonPlatform.FACEIT,
+    is_round_robin_bo2_as_2xbo1: false,
+    grand_final_round_one_only: false,
+    payment_link: null,
+    registration_price: null,
+    has_vat: false,
+    early_bird_price_discount: null,
+    early_bird_price_discount_end_date: null,
+    active_map_pool: [1, 2, 3],
+    rulebook_url: null,
+    discord_link: null,
+    ...overrides
+  } satisfies SeasonDetails;
+};
