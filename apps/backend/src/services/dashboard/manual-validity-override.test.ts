a8ba9cf6a9a7cb81d4f237b109b838e155ad6a1b
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2026-01-21 23:31:23 +0000

    Remove skips

--- apps/backend/src/services/dashboard/manual-validity-override.test.ts
+++ apps/backend/src/services/dashboard/manual-validity-override.test.ts
@@ -12,7 +12,7 @@ const mockGetConnection = getConnection as jest.MockedFunction<
   typeof getConnection
 >;
 
-describe.skip("manual validity override", () => {
+describe("manual validity override", () => {
   const mockConnection = {
     beginTransaction: jest.fn(),
     commit: jest.fn(),
