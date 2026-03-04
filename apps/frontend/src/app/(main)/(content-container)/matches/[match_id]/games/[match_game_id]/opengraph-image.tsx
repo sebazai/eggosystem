a8daaf788f3686a16d5613a2631cd3b5b4563a6a
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2026-03-04 17:24:04 +0000

    Fixes for opengraph

--- apps/frontend/src/app/(main)/(content-container)/matches/[match_id]/games/[match_game_id]/opengraph-image.tsx
+++ apps/frontend/src/app/(main)/(content-container)/matches/[match_id]/games/[match_game_id]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import {
   createMatchOgImageResponse,
   OG_IMAGE_SIZE
-} from "./opengraph-image.utils";
+} from "../../opengraph-image.utils";
 
 export const alt = "Match";
 export const size = OG_IMAGE_SIZE;
@@ -10,7 +10,7 @@ export const contentType = "image/png";
 export default async function Image({
   params
 }: {
-  params: Promise<{ match_id: string }>;
+  params: Promise<{ match_id: string; match_game_id: string }>;
 }) {
   const { match_id } = await params;
   const matchId = parseInt(match_id, 10);
